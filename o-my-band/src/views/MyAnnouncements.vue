a0e5ae2c6f4b3d96268dcfc8e77964da5410c14e
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-02-20 15:52:15 +0100

    added cards in myannoucenement page

--- o-my-band/src/views/MyAnnouncements.vue
+++ o-my-band/src/views/MyAnnouncements.vue
@@ -1,6 +1,66 @@
 <template>
   <HeaderComponent />
-  <div class="container">
+  <div
+        v-if="announcements">
+    <div v-for="announcement in allAnnouncements" :key="announcement.announcement_id"
+        class="card-backgroundColor border-1 surface-border border-round mt-8 mx-8 mb-8 text-center py-1 px-4"
+    >
+        <div class="flex justify-content-between">
+            <div class="flex flex-wrap align-items-start">
+                <div class="flex flex-column">
+                    <h2 class="">{{ announcement.title }}</h2>
+                    <div class="flex flex-wrap align-items-baseline">
+                        <img
+                            class="avatar"
+                            src="../assets/img/user-icon.png"
+                            alt="avatar"
+                        />
+                        <p v-if="announcement.user" class="ml-3">
+                            {{ announcement.user.pseudo }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-wrap">
+                <div v-for="style in announcement.styles" :key="style.style_id">
+                    <Tag class="tag h-1rem md:h-2rem mt-5 ml-1">{{
+                        style.name
+                    }}</Tag>
+                </div>
+                <Tag class="tag ml-2 h-1rem md:h-2rem mt-5">{{
+                    announcement.researchType
+                        ? announcement.researchType
+                        : ""
+                }}</Tag>
+            </div>
+        </div>
+        <div>
+            <p>{{ announcement.description }}</p>
+        </div>
+        <div class="flex justify-content-between align-items-baseline">
+            <p class="opacity-60">
+                Publiée le
+                {{
+                    new Date(announcement.created_at).toLocaleDateString(
+                        "fr-FR"
+                    ) || ""
+                }}
+            </p>
+            <p v-if="announcement.user" class="opacity-60">
+                {{ announcement.user.location }}
+            </p>
+
+            <Dialog
+                v-model:visible="visible"
+                modal
+                header="Répondre à l'annonce"
+                :style="{ width: '50rem' }"
+                :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+            >
+                <ContactForm />
+            </Dialog>
+        </div>
+    </div>
   </div>
   <FooterComponent />
 </template>
@@ -8,7 +68,8 @@
 <script>
 import FooterComponent from "@/components/FooterComponent.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
-import  { authStore } from "@/stores/auth.js"
+import  { authStore } from "@/stores/auth.js";
+import axios from "axios";
 
 export default {
   name: "HomeView",
@@ -20,12 +81,27 @@ export default {
         return {
             announcements: {},
             store: authStore(),
+            allAnnouncements: {}
         };
     },
     async created() {
         const user = await this.store.fetchProfil(this.store.jwToken);
         this.announcements = user.announcements;
-        console.log(this.announcements);
+        const myannouncements = await Promise.all(this.announcements.map(async(ad) => {
+          const anAnnouncement = await axios.get(`http://localhost:8080/announcements/${ad.announcement_id}`);
+          console.log(ad);
+          return {
+              id: ad.announcement_id,
+              title: anAnnouncement.data.title,
+              description: anAnnouncement.data.description,
+              instruments: anAnnouncement.data.instruments,
+              styles: anAnnouncement.data.styles,
+              researchType: anAnnouncement.data.researchType.name,
+              created_at: anAnnouncement.data.created_at
+
+          }
+        }));
+        this.allAnnouncements = myannouncements;
     },
 };
 </script>
